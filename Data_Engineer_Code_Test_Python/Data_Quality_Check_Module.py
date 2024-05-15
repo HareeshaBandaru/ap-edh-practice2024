42e439808de843afc270217536a08133c8658ab5
Hareesha-Bandaru <42827601+HareeshaBandaru@users.noreply.github.com>
2024-05-15 15:46:16 +0530

    fix:

--- Data_Engineer_Code_Test_Python/Data_Quality_Check_Module.py
+++ Data_Engineer_Code_Test_Python/Data_Quality_Check_Module.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 # Load data
-df = pd.read_csv("C:\\Users\hareesha.bandaru\Documents\GitHub\\ap-edh-practice2024\source_folder\data_file_20210527182730.csv")
+df = pd.read_csv(
+    "C:\\Users\hareesha.bandaru\Documents\GitHub\\ap-edh-practice2024\source_folder\data_file_20210527182730.csv")
 
 df.head(3)
